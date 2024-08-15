e11d57006d2c2742f8c59fbbe05b4c8fc9e3edf6
CyberneticNinja <AbrokwahS@gmail.com>
2024-08-14 21:45:08 -0400

    factory logic added and database seeder modified

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -17,7 +17,11 @@ class ItemFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'barcode' => $this->faker->unique()->ean13(), // Generates a random 13-digit barcode
+            'name' => $this->faker->word, // Generates a random word for the name
+            'quantity' => $this->faker->numberBetween(1, 100), // Random quantity between 1 and 100
+            'price' => $this->faker->randomFloat(2, 1, 1000), // Random price between 1.00 and 1000.00
+            'description' => $this->faker->sentence, // Generates a random sentence for description
         ];
     }
 }
