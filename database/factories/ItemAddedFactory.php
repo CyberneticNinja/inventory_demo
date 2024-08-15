778b2a23b8fffffc89513ee24056691446a3b396
CyberneticNinja <AbrokwahS@gmail.com>
2024-08-14 21:44:48 -0400

    factory logic added and database seeder modified

--- database/factories/ItemAddedFactory.php
+++ database/factories/ItemAddedFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Item;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ItemAdded>
@@ -16,8 +17,18 @@ class ItemAddedFactory extends Factory
      */
     public function definition(): array
     {
+        // Check if there are any existing Item records
+        $item = Item::inRandomOrder()->first();
+
+        // If no items exist, create a new one using the Item factory
+        if (!$item) {
+            $item = Item::factory()->create();
+        }
+
         return [
-            //
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'item_id' => $item->id,
+            'date' => $this->faker->date(),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
 
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Gabriel Grey',
+            'email' => 'admin@somecorp.com',
+        ]);
     }
 }
