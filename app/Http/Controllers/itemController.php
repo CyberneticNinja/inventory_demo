2a4e5d1101b6de71938fe6675904d937c06b405d
CyberneticNinja <AbrokwahS@gmail.com>
2024-08-31 01:50:57 -0400

    itemController => itemController

--- app/Http/Controllers/itemController.php
+++ app/Http/Controllers/itemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Item;
 
-class itemController extends Controller
+class ItemController extends Controller
 {
     public function index()
     {
